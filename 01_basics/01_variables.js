0261fa53d59d1ef5886944151705643b37b2fa19
Dhananjay Singh <140902867+Dhananjay-Singh1@users.noreply.github.com>
2023-10-01 08:41:08 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 14451
+let accountEmail = "dhannjay1104@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
