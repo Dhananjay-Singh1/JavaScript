8ab3572ce7e68fcef97695584decd96cdd67b725
Dhananjay Singh <140902867+Dhananjay-Singh1@users.noreply.github.com>
2023-10-02 12:24:08 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,13 @@
+let x = 3;
+let y = x++;
+console.log(x);
+console.log(y);
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
